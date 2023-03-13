cb01b17348bd7a974cb7cd040c712f7f077cbb3b
Andy KWAN <andykwanfai@gmail.com>
2023-03-13 22:18:21 +0800

    Create members.ts

---
+++ members.ts
@@ -0,0 +1,67 @@
+export enum NogiMember {
+  nogizaka46 = "乃木坂46",
+
+  Saito_Asuka = "齋藤飛鳥",
+
+  Suzuki_Ayane = "鈴木絢音",
+
+  Ito_Riria = "伊藤理々杏",
+  Iwamoto_Renka = "岩本蓮加",
+  Umezawa_Minami = "梅澤美波",
+  Kubo_Shiori = "久保史緒里",
+  Sakaguchi_Tamami = "阪口珠美",
+  Sato_Kaede = "佐藤楓",
+  Nakamura_Reno = "中村麗乃",
+  Mukai_Hazuki = "向井葉月",
+  Yamashita_Mizuki = "山下美月",
+  Yoshida_AyanoChristie = "吉田綾乃ｸﾘｽﾃｨｰ",
+  Yoda_Yuuki = "与田祐希",
+
+  Endo_Sakura = "遠藤さくら",
+  Kaki_Haruka = "賀喜遥香",
+  Kakehashi_Sayaka = "掛橋沙耶香",
+  Kanagawa_Saya = "金川紗耶",
+  Kitagawa_Yuri = "北川悠理",
+  Kuromi_Haruka = "黒見明香",
+  Sato_Rika = "佐藤璃果",
+  Shibata_Yuna = "柴田柚菜",
+  Seimiya_Rei = "清宮レイ",
+  Tamura_Mayu = "田村真佑",
+  Tsutsui_Ayame = "筒井あやめ",
+  Hayakawa_Seira = "早川聖来",
+  Hayashi_Runa = "林瑠奈",
+  Matsuo_Miyu = "松尾美佑",
+  Yakubo_Mio = "矢久保美緒",
+  Yumiki_Nao = "弓木奈於",
+
+  Ioki_Mao = "五百城茉央",
+  Ikeda_Teresa = "池田瑛紗",
+  Ichinose_Miku = "一ノ瀬美空",
+  Inoue_Nagi = "井上和",
+  Okamoto_Hina = "岡本姫奈",
+  Ogawa_Aya = "小川彩",
+  Okuda_Iroha = "奥田いろは",
+  Kawasaki_Sakura = "川﨑桜",
+  Sugawara_Satsuki = "菅原咲月",
+  Tomisato_Nao = "冨里奈央",
+  Nakanishi_Aruno = "中西アルノ",
+}
+export const NogiMembers: string[] = Object.values(NogiMember);
+
+export enum NogiGraduate {
+  Akimoto_Manatsu = "秋元真夏",
+  Ikuta_Erika = "生田絵梨花",
+  Nakada_Kana = "中田花奈",
+  Matsumura_Sayuri = "松村沙友理",
+  Saito_Chiharu = "斎藤ちはる",
+  Takayama_Kazumi = "高山一実",
+  Higuchi_Hina = "樋口日奈",
+  Wada_Maaya = "和田まあや",
+
+  Ito_Junna = "伊藤純奈",
+  Kitano_Hinako = "北野日奈子",
+  Shinuchi_Mai = "新内眞衣",
+  Watanabe_Miria = "渡辺みり愛",
+  Yamazaki_Rena = "山崎怜奈",
+}
+export const NogiGraduates: string[] = Object.values(NogiGraduate);
